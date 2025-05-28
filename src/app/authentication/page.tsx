45db70408e0b9e5dfea0e3cea8bc3c9e43858c6b
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-05-27 23:14:35 -0300

    feat: create login form component with validation using react-hook-form and zod

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,113 +1,23 @@
 "use client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+import { SignInForm } from "./components/sign-in-form";
 import { SignUpForm } from "./components/sign-up-form";
-
-const loginSchema = z.object({
-  email: z
-    .string()
-    .email("Email inválido.")
-    .trim()
-    .min(1, "Email é obrigatório."),
-  password: z.string().trim().min(8, "Senha deve ter pelo menos 8 caracteres."),
-});
-
 function AuthenticationPage() {
-  const loginForm = useForm({
-    resolver: zodResolver(loginSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  function loginSubmit(values: z.infer<typeof loginSchema>) {
-    console.log(values);
-    loginForm.reset();
-  }
-
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Tabs defaultValue="login" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="login">Login</TabsTrigger>
-          <TabsTrigger value="register">Criar conta</TabsTrigger>
+          <TabsTrigger className="cursor-pointer" value="login">
+            Login
+          </TabsTrigger>
+          <TabsTrigger className="cursor-pointer" value="register">
+            Criar conta
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="login">
-          <Card>
-            <Form {...loginForm}>
-              <form
-                onSubmit={loginForm.handleSubmit(loginSubmit)}
-                className="space-y-8"
-              >
-                <CardHeader>
-                  <CardTitle>Login</CardTitle>
-                  <CardDescription>
-                    Faça login com seu usuário e senha para continuar.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                  <FormField
-                    control={loginForm.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>E-mail</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Informe o seu email" {...field} />
-                        </FormControl>
-                        <FormMessage className="text-xs" />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={loginForm.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Senha</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="password"
-                            placeholder="Informe a sua senha"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage className="text-xs" />
-                      </FormItem>
-                    )}
-                  />
-                </CardContent>
-                <CardFooter>
-                  <Button type="submit" className="w-full">
-                    Entrar
-                  </Button>
-                </CardFooter>
-              </form>
-            </Form>
-          </Card>
+          <SignInForm />
         </TabsContent>
         <TabsContent value="register">
           <SignUpForm />
