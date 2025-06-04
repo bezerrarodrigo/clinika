be701a0e4c29e6c5fdc8d1aba864d47628ebbae6
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-06-03 21:38:29 -0300

    feat: add dayjs for time handling in upsertDoctorForm action with UTC conversion

--- src/actions/upsert-doctor-form/index.tsx
+++ src/actions/upsert-doctor-form/index.tsx
@@ -1,5 +1,7 @@
 "use server";
 
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import { headers } from "next/headers";
 
 import { db } from "@/db";
@@ -9,9 +11,26 @@ import { actionClient } from "@/lib/next-safe-action";
 
 import { upsertDoctorFormSchema } from "./schema";
 
+dayjs.extend(utc);
+
 export const upsertDoctorFormAction = actionClient
   .schema(upsertDoctorFormSchema)
   .action(async ({ parsedInput }) => {
+    const availableFromTime = parsedInput.availableFromTime;
+    const availableToTime = parsedInput.availableToTime;
+
+    const availableFromTimeUtc = dayjs()
+      .set("hour", parseInt(availableFromTime.split(":")[0]))
+      .set("minute", parseInt(availableFromTime.split(":")[1]))
+      .set("second", parseInt(availableFromTime.split(":")[2]))
+      .utc();
+
+    const availableToTimeUtc = dayjs()
+      .set("hour", parseInt(availableToTime.split(":")[0]))
+      .set("minute", parseInt(availableToTime.split(":")[1]))
+      .set("second", parseInt(availableToTime.split(":")[2]))
+      .utc();
+
     const session = await auth.api.getSession({
       headers: await headers(),
     });
@@ -27,12 +46,18 @@ export const upsertDoctorFormAction = actionClient
     await db
       .insert(doctorsTable)
       .values({
-        id: parsedInput.id,
         ...parsedInput,
+        id: parsedInput.id,
         clinicId: session.user.clinic.id,
+        availableFromTime: availableFromTimeUtc.format("HH:mm:ss"),
+        availableToTime: availableToTimeUtc.format("HH:mm:ss"),
       })
       .onConflictDoUpdate({
         target: [doctorsTable.id],
-        set: { ...parsedInput },
+        set: {
+          ...parsedInput,
+          availableFromTime: availableFromTimeUtc.format("HH:mm:ss"),
+          availableToTime: availableToTimeUtc.format("HH:mm:ss"),
+        },
       });
   });
